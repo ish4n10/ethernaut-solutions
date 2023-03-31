7b4367b1f0d36d3fa50af07695cdfc1daf0144ec
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2023-03-31 19:16:04 +0530

    Update solution.sol

--- Fal1out/solution.sol
+++ Fal1out/solution.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.8;
 
+// compile in remix and use the given contract's address.
 interface s {
     function Fal1out() external payable;
 
     function owner() external view returns (address);
 }
+// call Fal1out, then owner
