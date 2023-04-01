2cadf14b8bea4ac9a2516b64b7e6d804fb848231
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2023-04-02 00:09:42 +0530

    Update Force.sol

--- Force.sol
+++ Force.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8;
 
 
 contract expl {
-            // We just need a way to send the contract eth
-            // selfdestruct -> destroys itself and sends it's funds to given address 
+          	  // We just need a way to send the contract eth
+           	 // selfdestruct -> destroys itself and sends it's funds to given address 
 	function pwn() public {
 		selfdestruct(payable(0xa7BBD70BE89D56121e4c239f0AE54bc1511544D8));
 	}
